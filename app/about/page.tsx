2253dd94654f72cee754d506f47cdc88ffa370c2
Tom Mazzag <itstomazz@gmail.com>
2026-03-01 14:58:30 +0000

    feat: add homepage

---
+++ app/about/page.tsx
@@ -0,0 +1,120 @@
+import { ReactNode } from 'react';
+import { HighlightText } from '@MusicMe/components/Util/TextComponents';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  description:
+    'Find out more about the MusicMe platform. Learn about how you can share music, our long term goals and why you should use our platform.',
+};
+
+export default function AboutPage() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <nav className="navbar border-b-2 border-accent">
+        <div className="flex-1">
+          <a href="/" className="btn btn-ghost text-xl md:text-4xl">
+            MusicMe
+          </a>
+        </div>
+        <ul className="flex-none gap-2 inline-flex list-none">
+          <li>
+            <a href="/" className="btn btn-sm md:btn-md btn-ghost md:text-xl">
+              Login
+            </a>
+          </li>
+          <li>
+            <a href="/register" className="btn btn-sm md:btn-md btn-ghost md:text-xl">
+              Sign up for the beta
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="p-4 md:grid md:grid-cols-2 flex-1">
+        <TextTile title="What is MusicMe?">
+          <p>
+            MusicMe is an online social media platform aimed at allow users to share their music taste with others,
+            discover new music, gain and share insights into their music taste.
+          </p>
+        </TextTile>
+        <ImageTile src={''} />
+
+        <ImageTile src={''} />
+        <TextTile title="Why use our platform?">
+          <p>Our platform is a way to give your opinion on songs and give them a rating</p>
+          <br />
+          <p>
+            In the start we will aim to create as many new features as users feel necessary. We want to create a
+            platform for the music community
+          </p>
+          <br />
+          <p>Some of the things were actively working on are:</p>
+          <ul className="ml-3 list-disc">
+            <li>
+              <HighlightText bold={true}>Dedicated pages</HighlightText> for genres where you can find out about events
+              happening neaby, new trending music for a specific genre and some of the top reccomended songs recently
+              for a genre
+            </li>
+            <li>
+              Showing you <HighlightText bold={true}>live analytics</HighlightText> of what the top searched songs on
+              the platform are each day as well as the top artists and top reviews and providing you as much insight
+              into these songs as possible
+            </li>
+          </ul>
+        </TextTile>
+
+        <TextTile title="Future goals">
+          <p>As mentioned above we will be happy to listen to any product requests from users</p>
+          <br />
+          <p>Some of the goals we already have planned are:</p>
+          <ul className="ml-3 list-disc">
+            <li>Support for apple music</li>
+            <li>Greater profile analytics</li>
+            <li>Song suggestions using AI/ML</li>
+          </ul>
+        </TextTile>
+        <ImageTile src={''} />
+      </div>
+
+      <footer className="md:mt-10">
+        <div className="bg-base-300 flex justify-between px-10 py-8">
+          Created by Thomas Mazzag
+          <div aria-label="links" className="flex gap-2">
+            <a href="https://www.linkedin.com/in/thomas-mazzag/" className="linkedin" target="_blank">
+              <i className="fa-brands fa-linkedin fa-2xl" aria-hidden="true"></i>
+            </a>
+            <a href="https://github.com/TomMazzag" className="github" target="_blank">
+              <i className="fa-brands fa-github fa-2xl" aria-hidden="true"></i>
+            </a>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+interface TextTileProps {
+  title: string;
+  children?: ReactNode;
+}
+
+const TextTile = ({ title, children }: TextTileProps) => {
+  return (
+    <div className="bg-base-300 px-10 py-6 rounded-xl md:mx-14 my-6">
+      <h1 className="text-accent text-3xl mb-2">{title}</h1>
+      {children}
+    </div>
+  );
+};
+
+interface ImageTileProps {
+  src: string;
+}
+
+const ImageTile = ({ src }: ImageTileProps) => {
+  return (
+    <div className="flex items-center md:mx-14 my-6 md:px-10  py-2 md:py-6 rounded-xl border border-accent">
+      <img src={src} alt="" />
+    </div>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,39 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
+  const SPOTIFY_CLIENT_ID = '5c9346c993f3470284a7ae8623242385';
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="h-screen">
+      <div className="flex md:grid md:grid-cols-2 h-full overflow-hidden justify-center">
+        <div className="flex flex-col justify-evenly items-center">
+          <div className="text-center mt-10">
+            <h1 className="text-6xl font-bold mb-4">MusicMe</h1>
+            <p className="text-xl italic">Social media for music</p>
+          </div>
+
+          <div className="flex flex-col gap-6 text-center">
+            <button className="btn btn-outline btn-primary">Click here to begin</button>
+
+            <div className="w-full mt-8">
+              <p className="pb-2 opacity-75">
+                Dont have an account? <br></br> Sign up for the beta below!
+              </p>
+              <button className="btn btn-outline w-full">Sign up for the beta</button>
+            </div>
+          </div>
+          <a href="/about" className="text-accent">
+            About
+          </a>
+        </div>
+
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+          src="/platform.webp"
+          alt="Platform image"
+          width={1920}
+          height={1080}
+          className="h-full border-l-4 border-accent object-cover object-right hidden md:block"
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
     </div>
   );
 }
