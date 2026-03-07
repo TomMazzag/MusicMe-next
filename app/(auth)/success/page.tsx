97daec634fe2e457a219daa48a372097b1157ff9
Tom Mazzag <itstomazz@gmail.com>
2026-03-07 19:03:52 +0000

    feat: update sucess page, add account skeleton

--- app/(auth)/success/page.tsx
+++ app/(auth)/success/page.tsx
@@ -1,29 +1,38 @@
-import { generateSpotifyToken } from '@MusicMe/lib/spotify';
+'use client';
+
+import { LoginButton } from '@MusicMe/components/Login/LoginButton';
+import { useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
 import { PropagateLoader } from 'react-spinners';
 
-export default async function Success({ 
-  searchParams 
-}: { 
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
-}) {
-  const params = await searchParams;
-  const code = params.code as string;
-  const state = params.state as string;
-  if (!code || !state) {
-    throw new Error('Error getting spotify code');
-  }
+export default function Success() {
+  const searchParams = useSearchParams();
+  const code = searchParams.get('code');
+  const state = searchParams.get('state');
 
-  const spotifyToken = await generateSpotifyToken(code, state);
+  useEffect(() => {
+    if (code && state) {
+      const redirectUrl = `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/callback?code=${code}&state=${state}`;
+      window.location.href = redirectUrl;
+    }
+  }, [code, state]);
+
+  if (!code || !state) {
+    return (
+      <div className="success-redirect flex flex-col justify-center items-center h-screen text-center gap-12">
+        <h1>Error</h1>
+        <p>Missing code or state parameters.</p>
+
+        <p>Please try again.</p>
+        <LoginButton />
+      </div>
+    );
+  }
 
   return (
     <div className="success-redirect flex flex-col justify-center items-center h-screen text-center gap-12">
       <h1>Welcome to social media for music!</h1>
-
-      <p>Loading...</p>
-
-      {code && <p>Code: {code}</p>}
-      {state && <p>State: {state}</p>}
-
+      <p>Redirecting...</p>
       <PropagateLoader color="lightgreen" style={{ display: 'inherit', position: 'relative', left: '-7px' }} />
     </div>
   );
