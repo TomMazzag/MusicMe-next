054c9c3b4d59d43039bec0b2e1fc712e21c8403e
Tom Mazzag <itstomazz@gmail.com>
2026-05-20 13:31:43 +0100

    feat: add suspense for useSearchParams

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,19 +1,22 @@
 import { Metadata } from 'next';
 import SearchClientSide from './components/SearchClientSide';
 import { Navbar } from '@MusicMe/components/Navbar/Navbar';
+import { Suspense } from 'react';
+import { ScaleLoader } from 'react-spinners';
 
 export const metadata: Metadata = {
   title: 'MusicMe | Search',
-  description:
-    'Search for new tracks, follow friends or find new Artists!',
+  description: 'Search for new tracks, follow friends or find new Artists!',
 };
 
 export default function SearchPage() {
   return (
     <>
       <Navbar />
-      <div className='flex flex-col items-center gap-8 py-8'>
-        <SearchClientSide />
+      <div className="flex flex-col items-center gap-8 py-8">
+        <Suspense fallback={<ScaleLoader color={'#22c55e'} />}>
+          <SearchClientSide />
+        </Suspense>
       </div>
     </>
   );
